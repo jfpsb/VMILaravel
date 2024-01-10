fb1f3c13ee8fbf917d3edcedfcc1761f5f408b36
Felipe Borges <jfpsb_felipe2@hotmail.com>
2024-01-09 22:11:50 -0300

    Início Tela de login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('css/global.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}" />
+    <title>Vanda Moda Íntima - Login</title>
+</head>
+
+<body>
+    <form>
+        <div class="login">
+            <label>Usuário:
+                <br />
+                <input type="text" />
+            </label>
+            <label>Senha:
+                <br />
+                <input type="password" />
+            </label>
+        </div>
+        <div id="botao-wrapper">
+            <button id="botao-login">Confirmar</button>
+        </div>
+    </form>
+</body>
+
+</html>
